74aff8334f3e9b851b1fa0b32bd9eb3588997087
Hanoona Rasheed <hanoona@uniqueroboticsedu.com>
2020-07-04 17:31:04 +0400

    10 added

---
+++ assignment_010/main.py
@@ -0,0 +1,5 @@
+# assignment_010: Building your own Data set from Google Images
+# The task is to create an image dataset through Google Images, visualise the train and
+# test sets.
+
+# Your codes here..
